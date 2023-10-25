c3bb2068cdd13854df21e944bebf07f6ba7f05cf
iumutc <96541001+iumutc@users.noreply.github.com>
2023-10-25 20:41:46 +0300

    Minor Arrangements

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    public Transform target; // The reference to the object to follow
-    public float followSpeed = 5f; // How fast the camera follows the target
-     public Vector2 minBoundaries, maxBoundaries; // Sınırların değerleri (minX, maxX, minY, maxY olarak da kullanılabilir)
+    public Transform target; 
+    public float followSpeed = 5f; 
+     public Vector2 minBoundaries, maxBoundaries;
 
     void Update()
     {
         if (target != null)
         {
-            // Hedefin pozisyonunu sınırla
+            
             float clampedX = Mathf.Clamp(target.position.x, minBoundaries.x, maxBoundaries.x);
             float clampedY = Mathf.Clamp(target.position.y, minBoundaries.y, maxBoundaries.y);
 
-            // Kameranın yeni pozisyonunu belirle
+            
             Vector3 targetPosition = new Vector3(clampedX, clampedY, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
         }
