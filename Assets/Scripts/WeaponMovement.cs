bd22c0e0b024b2cf31f1e535b570120970efbf80
kutayberrak <127015869+kutayberrak@users.noreply.github.com>
2023-11-11 19:37:30 +0300

    Character shooting added
    There are some bugs. Later i will fix them.

--- Assets/Scripts/WeaponMovement.cs
+++ Assets/Scripts/WeaponMovement.cs
@@ -5,6 +5,10 @@ public class WeaponMovement : MonoBehaviour
     private Vector3 mousePosition;
     public float rotationSpeed = 100f;
 
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+
     private void Start()
     {
         weaponTransform = this.transform;
@@ -12,9 +16,14 @@ public class WeaponMovement : MonoBehaviour
 
     private void Update()
     {
+        aiming();
+        shooting();
+    }
+
+    private void aiming() {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = mousePosition-transform.position;
-        float rotz = Mathf.Atan2(rotation.y, rotation.x)*Mathf.Rad2Deg;
+        Vector3 rotation = mousePosition - transform.position;
+        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
         if (mousePosition.x < weaponTransform.position.y)
         {
@@ -23,6 +32,32 @@ public class WeaponMovement : MonoBehaviour
         else
         {
             transform.rotation = Quaternion.Euler(0, 0, rotz);
+        }
+    }
+
+    private void shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            Vector3 direction = (mousePosition - bulletSpawn.position).normalized;
+
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+            Destroy(bullet, 1f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
